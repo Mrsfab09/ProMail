05957decb3d8bf739546f95bb955f54b67c305d8
MrsFab09 <ytfabiskora07@gmail.com>
2024-06-30 15:26:20 +0200

    add form sign up

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
   return (
     <div className="container">
       <Brand />
-      <Form formName={"Login"} value={"Sign Up"} />
+      <Form
+        formName={"Login"}
+        value={"Sign Up"}
+        question={"You dont have an account?"}
+      />
+      {/* <Form
+        formName={"Sign Up"}
+        value={"Login"}
+        question="You already have an account?"
+      /> */}
     </div>
   );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,17 +2,17 @@ import styles from "./Form.module.css";
 import { Input } from "../Input/Input";
 import { Button } from "../Button/Button";
 
-export function Form({ formName, value }) {
+export function Form({ formName, value, question }) {
   return (
     <div className={styles.wrapForm}>
-      <form className={styles.form}>
+      <form className={styles.form} method="POST" action="">
         <h2>{formName}</h2>
         <Input name="Login" />
         <Input name="Password" />
-        <Button name={"Login in"} />
+        <Button name="Login in" />
       </form>
       <span className={styles.span}>
-        You dont have an account? <a href="">{value}</a>
+        {question} <a href="">{value}</a>
       </span>
     </div>
   );
