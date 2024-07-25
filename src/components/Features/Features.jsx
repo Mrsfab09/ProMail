4e17ebf6c547fb9498897346f30b1c3c25ab9e67
MrsFab09 <ytfabiskora07@gmail.com>
2024-07-25 23:32:39 +0200

    adding few style in the fetures section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -5,6 +5,11 @@ export function Features() {
     <section className={styles.sectionFeatures}>
       <div className={styles.featuresContent}>
         <h2>Features</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+          accusantium voluptatum cupiditate qui vel modi quia nostrum ea libero
+          perferendis!
+        </p>
       </div>
       <div></div>
     </section>
